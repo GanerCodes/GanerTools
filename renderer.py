48fe5e28721a07d2a492bf601bf99b830a5c6351
GanerCodes <ganercrafter@gmail.com>
2022-06-05 17:50:02 -0500

    Add files

--- renderer.py
+++ renderer.py
@@ -32,10 +32,21 @@ class Renderer(moderngl_window.WindowConfig):
         super().__init__(**kwargs)
         self.generate_vao()
     
+    def parse_shader(q):
+        body = f"""\
+        vec4 func(vec2 p) {{
+            {q[(l:=q.find('MAIN:'))+5:]}
+        }}
+        void main() {{
+            gl_FragColor = func(pos);
+        }}"""
+        head = q[:l]
+
+        return f"""{BLOCK}\n{head}\n{body}"""
+    
     def exec(shader, filename="output.png"):
-        print(shader)
         Renderer.filename = filename
-        Renderer.shader = shader
+        Renderer.shader = Renderer.parse_shader(shader)
         moderngl_window.run_window_config(Renderer)
     
     def render(self, time, frame_time):
@@ -64,18 +75,6 @@ float pml(float x,float a,float b){float t=abs(b-a);return mod(-x*sign(mod(floor
 vec2 ptc(float d,float a){return vec2(d*cos(a),d*sin(a));}
 vec3 hsv(vec3 c){vec4 K=vec4(0.0,-1.0/3.0,2.0/3.0,-1.0),p=mix(vec4(c.bg,K.ab),vec4(c.gb,K.rg),step(c.b,c.g)),q=mix(vec4(p.rga,c.r),vec4(c.r,p.gbr),step(p.r,c.r));float d=q.r-min(q.a,q.g),e=1e-10;return vec3(abs(q.b+(q.a-q.g)/(6.0*d+e)),d/(q.r+e),q.r);}
 vec3 rgb(vec3 c){vec4 K=vec4(1.0,0.66666,0.33333,3.0);vec3 p=abs(fract(c.rrr+K.rgb)*6.0-K.aaa);return c.b*mix(K.rrr,p-K.rrr,c.g);}"""
-
-def parse_shader(q):
-    body = f"""\
-    vec4 func(vec2 p) {{
-        {q[(l:=q.find('MAIN:'))+5:]}
-    }}
-    void main() {{
-        gl_FragColor = func(pos);
-    }}"""
-    head = q[:l]
-
-    return f"""{BLOCK}\n{head}\n{body}"""
 
 # Renderer.exec(parse_shader("""
 
